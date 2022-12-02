cc839c671619d308c52afa9a051ad06453a23dea
Pratibha Sazawal <117441697+Pratibha-Sazawal@users.noreply.github.com>
2022-12-02 20:19:17 +0530

    Add files via upload

---
+++ Housing_Data_Cleaning.sql
@@ -0,0 +1,270 @@
+/*
+Cleaning Data in SQL Queries
+*/
+
+Select *
+From PortfolioProject.dbo.NashvilleHousing
+
+--------------------------------------------------------------------------------------------------------------------------
+
+-- Standardize Date Format
+
+Select SaleDate, Convert(Date, SaleDate)
+From PortfolioProject.dbo.NashvilleHousing
+
+Update PortfolioProject.dbo.NashvilleHousing    -- Doesn't update
+Set SaleDate = Convert(Date, SaleDate)
+
+Alter Table PortfolioProject.dbo.NashvilleHousing
+Add SaleDateConverted Date;
+
+Update PortfolioProject.dbo.NashvilleHousing
+Set SaleDateConverted = Convert(Date, SaleDate)
+
+Select SaleDateConverted
+From PortfolioProject.dbo.NashvilleHousing
+
+ --------------------------------------------------------------------------------------------------------------------------
+
+-- Populate Property Address data
+
+Select *
+From PortfolioProject.dbo.NashvilleHousing
+Where PropertyAddress Is Null
+
+Select *
+From PortfolioProject.dbo.NashvilleHousing
+Order By ParcelID         -- When Parcel ID is same for two entries, then Property Address is also same, but Unique ID is different
+-- Can populate the null property addresses using this
+
+-- Need to join the table with itself to match the column entries
+
+Select a.ParcelID, a.PropertyAddress, b.ParcelID, B.PropertyAddress
+From PortfolioProject.dbo.NashvilleHousing a
+Join PortfolioProject.dbo.NashvilleHousing b
+On a.ParcelID = b.ParcelID
+And a.[UniqueID ] <> b.[UniqueID ]
+Where a.PropertyAddress Is Null
+
+Select a.ParcelID, a.PropertyAddress, b.ParcelID, B.PropertyAddress, ISNULL(a.PropertyAddress, b.PropertyAddress)
+From PortfolioProject.dbo.NashvilleHousing a   -- Populating a.PropertyAddress where ever it's null by b.PropertyAddress
+Join PortfolioProject.dbo.NashvilleHousing b
+On a.ParcelID = b.ParcelID
+And a.[UniqueID ] <> b.[UniqueID ]
+Where a.PropertyAddress Is Null
+
+Update a
+Set PropertyAddress = ISNULL(a.PropertyAddress, b.PropertyAddress)
+From PortfolioProject.dbo.NashvilleHousing a
+Join PortfolioProject.dbo.NashvilleHousing b
+On a.ParcelID = b.ParcelID
+And a.[UniqueID ] <> b.[UniqueID ]
+Where a.PropertyAddress Is Null
+
+--------------------------------------------------------------------------------------------------------------------------
+
+-- Breaking out Address into Individual Columns (Address, City, State)
+
+Select PropertyAddress                    -- Address and City are separated by a comma everywhere
+From PortfolioProject.dbo.NashvilleHousing
+
+Select
+SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress)-1) As Address 
+, SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress)+1, LEN(PropertyAddress)) As Address
+From PortfolioProject.dbo.NashvilleHousing
+-- Substring(Where to take string from, starting point index, till where to look)
+--charindex gives position of the character
+-- -1 because we don't want the comma
+
+-- To separate two values from a column, we need to create two new columns
+
+Alter Table PortfolioProject.dbo.NashvilleHousing
+Add PropertySplitAddress Nvarchar(255);
+
+Update PortfolioProject.dbo.NashvilleHousing
+Set PropertySplitAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress)-1)
+
+Alter Table PortfolioProject.dbo.NashvilleHousing
+Add PropertySplitCity Nvarchar(255);
+
+Update PortfolioProject.dbo.NashvilleHousing
+Set PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress)+1, LEN(PropertyAddress))
+
+Select *
+From PortfolioProject.dbo.NashvilleHousing
+
+-- Easier way than Substring
+
+Select OwnerAddress
+From PortfolioProject.dbo.NashvilleHousing  -- Address, City and State are separated by commas everywhere
+
+-- Using Parsename - It is only useful with periods (.)
+-- Replace (,) with (.)
+
+Select
+PARSENAME(Replace(OwnerAddress, ',', '.'), 3)  -- 1 gives the first object separated by comma (state, since Parsename looks backwards)
+, PARSENAME(Replace(OwnerAddress, ',', '.'), 2)
+, PARSENAME(Replace(OwnerAddress, ',', '.'), 1)
+From PortfolioProject.dbo.NashvilleHousing
+
+Alter Table PortfolioProject.dbo.NashvilleHousing
+Add OwnerSplitAddress Nvarchar(255)
+
+Update PortfolioProject.dbo.NashvilleHousing
+Set OwnerSplitAddress = PARSENAME(Replace(OwnerAddress, ',', '.'), 3)
+
+Alter Table PortfolioProject.dbo.NashvilleHousing
+Add OwnerSplitCity Nvarchar(255)
+
+Update PortfolioProject.dbo.NashvilleHousing
+Set OwnerSplitCity = PARSENAME(Replace(OwnerAddress, ',', '.'), 2) 
+
+Alter Table PortfolioProject.dbo.NashvilleHousing
+Add OwnerSplitState Nvarchar(255)
+
+Update PortfolioProject.dbo.NashvilleHousing
+Set OwnerSplitState = PARSENAME(Replace(OwnerAddress, ',', '.'), 1)
+
+Select *
+From PortfolioProject.dbo.NashvilleHousing
+
+--------------------------------------------------------------------------------------------------------------------------
+
+-- Change Y and N to Yes and No in "Sold as Vacant" field
+
+Select Distinct(SoldAsVacant), Count(SoldAsVacant)
+From PortfolioProject.dbo.NashvilleHousing
+Group By SoldAsVacant
+Order By 2
+
+Select SoldAsVacant
+, Case When SoldAsVacant = 'Y' Then 'Yes'
+       When SoldAsVacant = 'N' Then 'No'
+	   Else SoldAsVacant
+	   End 
+From PortfolioProject.dbo.NashvilleHousing
+
+Update PortfolioProject.dbo.NashvilleHousing
+Set SoldAsVacant = Case When SoldAsVacant = 'Y' Then 'Yes'
+						When SoldAsVacant = 'N' Then 'No'
+						Else SoldAsVacant
+						End
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Remove Duplicates
+
+Select *
+From PortfolioProject.dbo.NashvilleHousing
+
+--Removing duplicate rows
+
+Select *,                      -- Partitioning data on things that are unique to each row
+	ROW_NUMBER() Over (
+	Partition By ParcelID,
+				 PropertyAddress,
+				 SaleDate,
+				 SalePrice,
+				 LegalReference
+				 Order By UniqueID
+				 ) row_num     -- row_num = 1 is a particular row and row_num = 2 is its duplicate
+From PortfolioProject.dbo.NashvilleHousing
+Order By ParcelID
+--Where row_num > 1    --Where won't work here, need to create a CTE
+
+  --Creating a CTE - common table expression - Temp table
+With RowNumCTE As(
+Select *,                      
+	ROW_NUMBER() Over (
+	Partition By ParcelID,
+				 PropertyAddress,
+				 SaleDate,
+				 SalePrice,
+				 LegalReference
+				 Order By UniqueID
+				 ) row_num     
+From PortfolioProject.dbo.NashvilleHousing
+)
+Select *        -- Querying data from this temp table
+From RowNumCTE
+Where row_num > 1
+Order By PropertyAddress  -- These are all duplicates
+
+-- Deleting duplicates
+With RowNumCTE As(
+Select *,                      
+	ROW_NUMBER() Over (
+	Partition By ParcelID,
+				 PropertyAddress,
+				 SaleDate,
+				 SalePrice,
+				 LegalReference
+				 Order By UniqueID
+				 ) row_num     
+From PortfolioProject.dbo.NashvilleHousing
+)
+Delete 
+From RowNumCTE
+Where row_num > 1
+
+---------------------------------------------------------------------------------------------------------
+
+-- Delete Unused Columns
+
+Select *
+From PortfolioProject.dbo.NashvilleHousing
+
+Alter Table PortfolioProject.dbo.NashvilleHousing
+Drop Column PropertyAddress, SaleDate, OwnerAddress, TaxDistrict
+
+
+
+-----------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------
+
+--- Importing Data using OPENROWSET and BULK INSERT	
+
+--  More advanced and looks cooler, but have to configure server appropriately to do correctly
+--  Wanted to provide this in case you wanted to try it
+
+
+--sp_configure 'show advanced options', 1;
+--RECONFIGURE;
+--GO
+--sp_configure 'Ad Hoc Distributed Queries', 1;
+--RECONFIGURE;
+--GO
+
+
+--USE PortfolioProject 
+
+--GO 
+
+--EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'AllowInProcess', 1 
+
+--GO 
+
+--EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'DynamicParameters', 1 
+
+--GO 
+
+
+---- Using BULK INSERT
+
+--USE PortfolioProject;
+--GO
+--BULK INSERT nashvilleHousing FROM 'C:\Temp\SQL Server Management Studio\Nashville Housing Data for Data Cleaning Project.csv'
+--   WITH (
+--      FIELDTERMINATOR = ',',
+--      ROWTERMINATOR = '\n'
+--);
+--GO
+
+
+---- Using OPENROWSET
+--USE PortfolioProject;
+--GO
+--SELECT * INTO nashvilleHousing
+--FROM OPENROWSET('Microsoft.ACE.OLEDB.12.0',
+--    'Excel 12.0; Database=C:\Users\alexf\OneDrive\Documents\SQL Server Management Studio\Nashville Housing Data for Data Cleaning Project.csv', [Sheet1$]);
+--GO
